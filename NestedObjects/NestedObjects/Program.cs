3edb461604a5a002cb0de90c1620c8997d7d9ab2
Bkc4444 <bkc4444@gmail.com>
2022-07-21 20:53:33 -0700

    Added test code

--- NestedObjects/NestedObjects/Program.cs
+++ NestedObjects/NestedObjects/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new()
+{
+    Email = "Annemarie@cptc.edu",
+    FullName = "Annemarie Smith",
+    OfficeLocation = "B17 Rm 150"
+};
+
+Student stu1 = new()
+{
+    FirstName = "Homer",
+    LastName = "Simpson",
+    DateOfBirth =  new DateOnly(2000, 3, 20),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "2535895580",
+    SchoolEmail = "Homer.Simpson@students.sptc.edu"
+};
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
